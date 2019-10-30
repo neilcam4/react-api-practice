284ad65cbdbaf3cb7d16332da9592ba35d5faf2d
Neil Campbell <neildcampbell@hotmail.com>
2019-10-30 05:48:52 +0000

    added client side routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {Component}from 'react';
 import './App.css';
 import ConsumeLess from './ConsumeLess'
 import Form from './Form'
 
-function App() {
+
+class App extends Component{
+
+constructor(props){
+  super(props);
+  this.state = {page:'about'};
+}
+changePage(newPage){
+  this.setState({page:newPage})
+}
+renderPage(){
+  if(this.state.page === 'form') return <Form />
+  if(this.state.page === 'consumeLess') return <ConsumeLess />
+}
+render(){
   return (
     <div className="App">
-      < ConsumeLess />     
-        <Form/>
+      <nav>
+        <a onClick={() => this.changePage('consumeLess')}>ConsumeLess</a>
+        <a onClick={() => this.changePage('form')}>Form</a>
+      </nav>
+      {this.renderPage()}
       </div>
   );
+  }
 }
-
 export default App;
